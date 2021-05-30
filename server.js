e7bc8ddbf7353d424c38c931fc903ec8e8319b57
alexrilo <alejandroriloferreiro@gmail.com>
2021-05-30 04:20:03 +0200

    xd

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/soloQChallengeProject'));
+app.use(express.static('src\app\dist\SoloQChallengeProject'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/soloQChallengeProject/'}),
+    res.sendFile('index.html', {root: 'src\app\dist\SoloQChallengeProject'}),
 );
 
 // Start the app by listening on the default Heroku port
